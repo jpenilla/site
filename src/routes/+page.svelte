ff5837d5ac6cc0440d773ac105b928c5e7090d0e
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-04-18 10:31:15 -0700

    Maybe fix ASU logo alignment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,9 @@
 <h2 class="text-xl font-semibold">Education</h2>
 <div class="card m-2 bg-base-200">
   <div class="card-body">
-    <ASULogo class="h-8 w-fit" />
+    <div class="flex">
+      <ASULogo class="h-8 w-fit" />
+    </div>
     <div class="flex flex-col">
       <span class="font-semibold">W.P. Carey School of Business</span>
       <span>Bachelor of Science in Computer Information Systems, Graduating Fall 2025</span>
