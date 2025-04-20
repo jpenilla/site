bc02808c0d1ef473e70f2824195ebdec98594007
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2025-04-20 13:36:47 -0700

    namespace technologies

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,16 +1,14 @@
 import { Link, ProjectGroup, ProjectInfo, Tech } from "./types";
 
-const gradle = new Tech("Gradle", "iconify logos--gradle bg-gradle", "https://gradle.org");
-const bun = new Tech("Bun", "iconify-color logos--bun", "https://bun.sh");
-const svelteKit = new Tech("SvelteKit", "iconify-color logos--svelte-icon", "https://svelte.dev");
-const tailwind = new Tech("Tailwind CSS", "iconify-color logos--tailwindcss-icon", "https://tailwindcss.com");
-const java = new Tech("Java", "iconify-color logos--java", "https://java.com");
-const kotlin = new Tech("Kotlin", "iconify-color logos--kotlin-icon", "https://kotlinlang.org");
-const springBoot = new Tech(
-  "Spring Boot",
-  "iconify-color logos--spring-icon",
-  "https://spring.io/projects/spring-boot",
-);
+const tech = {
+  gradle: new Tech("Gradle", "iconify logos--gradle bg-gradle", "https://gradle.org"),
+  bun: new Tech("Bun", "iconify-color logos--bun", "https://bun.sh"),
+  svelteKit: new Tech("SvelteKit", "iconify-color logos--svelte-icon", "https://svelte.dev"),
+  tailwind: new Tech("Tailwind CSS", "iconify-color logos--tailwindcss-icon", "https://tailwindcss.com"),
+  java: new Tech("Java", "iconify-color logos--java", "https://java.com"),
+  kotlin: new Tech("Kotlin", "iconify-color logos--kotlin-icon", "https://kotlinlang.org"),
+  springBoot: new Tech("Spring Boot", "iconify-color logos--spring-icon", "https://spring.io/projects/spring-boot"),
+};
 
 export const gradlePlugins: ProjectInfo[] = [
   new ProjectInfo(
@@ -19,7 +17,7 @@ export const gradlePlugins: ProjectInfo[] = [
     "resource-factory",
     "Gradle plugin for generating resources at build time",
     [Link.gppSearch("xyz.jpenilla.resource-factory")],
-    [kotlin],
+    [tech.kotlin],
   ),
   new ProjectInfo(
     "gremlin",
@@ -27,7 +25,7 @@ export const gradlePlugins: ProjectInfo[] = [
     "gremlin",
     "Gradle plugin and Java library for resolving Gradle Configurations at runtime",
     [Link.gpp("xyz.jpenilla.gremlin-gradle")],
-    [java, kotlin],
+    [tech.java, tech.kotlin],
   ),
   new ProjectInfo(
     "paperweight",
@@ -35,7 +33,7 @@ export const gradlePlugins: ProjectInfo[] = [
     "paperweight",
     "Gradle plugin for building the Paper Minecraft server software, forks of Paper, and Paper plugins",
     [Link.gppSearch("io.papermc.paperweight")],
-    [kotlin],
+    [tech.kotlin],
   ),
   new ProjectInfo(
     "run-task",
@@ -43,7 +41,7 @@ export const gradlePlugins: ProjectInfo[] = [
     "run-task",
     "Gradle plugins adding tasks to run Minecraft server and proxy software",
     [Link.gppSearch("xyz.jpenilla.run-")],
-    [kotlin],
+    [tech.kotlin],
   ),
   new ProjectInfo(
     "hangar-publish-plugin",
@@ -51,7 +49,7 @@ export const gradlePlugins: ProjectInfo[] = [
     "hangar-publish-plugin",
     "Gradle plugin for automated publishing to <a href='https://hangar.papermc.io' class='link'>Hangar</a>",
     [Link.gpp("io.papermc.hangar-publish-plugin")],
-    [java, kotlin],
+    [tech.java, tech.kotlin],
   ),
 ];
 
@@ -62,7 +60,7 @@ export const webApps: ProjectInfo[] = [
     "patch-roulette",
     "Featureful and performant multi-file diff viewer",
     [],
-    [svelteKit, tailwind, bun, java, springBoot, gradle],
+    [tech.svelteKit, tech.tailwind, tech.bun, tech.java, tech.springBoot, tech.gradle],
   ),
   new ProjectInfo(
     "site",
@@ -70,7 +68,7 @@ export const webApps: ProjectInfo[] = [
     "site",
     "Personal portfolio website (this website)",
     [],
-    [svelteKit, tailwind, bun],
+    [tech.svelteKit, tech.tailwind, tech.bun],
   ),
 ];
 
@@ -81,7 +79,7 @@ export const libraries: ProjectInfo[] = [
     "cloud",
     "Command framework & dispatcher for the JVM",
     [],
-    [java, kotlin, gradle],
+    [tech.java, tech.kotlin, tech.gradle],
   ),
   new ProjectInfo(
     "reflection-remapper",
@@ -89,7 +87,7 @@ export const libraries: ProjectInfo[] = [
     "reflection-remapper",
     "Java reflection library with support for obfuscation mappings",
     [],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
 ];
 
@@ -100,7 +98,7 @@ export const minecraftMods: ProjectInfo[] = [
     "squaremap",
     "World map renderer and web viewer for Minecraft servers",
     [Link.modrinth("squaremap")],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
   new ProjectInfo(
     "ChessCraft",
@@ -108,7 +106,7 @@ export const minecraftMods: ProjectInfo[] = [
     "chesscraft",
     "Adds in-world chess matches between players and or CPUs",
     [Link.modrinth("chesscraft")],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
   new ProjectInfo(
     "TabTPS",
@@ -116,7 +114,7 @@ export const minecraftMods: ProjectInfo[] = [
     "TabTPS",
     "Utility to monitor real-time server performance in-game",
     [Link.modrinth("tabtps")],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
   new ProjectInfo(
     "MiniMOTD",
@@ -124,7 +122,7 @@ export const minecraftMods: ProjectInfo[] = [
     "MiniMOTD",
     "Allows customizing the server icon and 'message of the day' text (shown on the server list) using <a href='https://docs.advntr.dev/minimessage/index.html' class='link' target='_blank'>MiniMessage</a>",
     [Link.modrinth("minimotd")],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
   new ProjectInfo(
     "Better Fabric Console",
@@ -132,7 +130,7 @@ export const minecraftMods: ProjectInfo[] = [
     "better-fabric-console",
     "Server-side Fabric mod enhancing the console with tab completions, colored log output, command syntax highlighting, command history, and more",
     [Link.modrinth("better-fabric-console")],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
   new ProjectInfo(
     "Mods Command",
@@ -140,7 +138,7 @@ export const minecraftMods: ProjectInfo[] = [
     "mods-command",
     "Fabric mod adding commands to list, search, and get information about installed mods",
     [Link.modrinth("mods-command")],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
   new ProjectInfo(
     "CarbonChat",
@@ -148,7 +146,7 @@ export const minecraftMods: ProjectInfo[] = [
     "Carbon",
     "Featureful chat enhancement mod with support for multiple mod loaders and the <a href='https://papermc.io/software/velocity' class='link' target='_blank'>Velocity</a> proxy, cross-server chat, an extensible API, and more",
     [Link.modrinth("carbon")],
-    [java, gradle],
+    [tech.java, tech.gradle],
   ),
 ];
 
